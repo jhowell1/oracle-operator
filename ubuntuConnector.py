d2278d474ddd053efd2317e19226bc48acfe18de
jhowell1 <56095374+jhowell1@users.noreply.github.com>
2019-11-08 18:12:54 -0800

    Update ubuntuConnector.py

--- ubuntuConnector.py
+++ ubuntuConnector.py
@@ -1,9 +1,10 @@
 from tkinter import *
+import oracleConnection as oc
 
 
 class UbuntuConnector (Frame):
 
-    server = ""
+    ip = ""
     username = ""
     password = ""
 
@@ -18,27 +19,34 @@ class UbuntuConnector (Frame):
 
         Label(window, text="IP Address: ").grid(row=0, column=0, sticky=E)
 
-        self.server = StringVar()
-        self.server = Entry(window).grid(row=0, column=1, sticky=W)
+        #self.ip = StringVar()
+        self.ip = Entry(window)
+        self.ip.grid(row=0, column=1, sticky=W)
 
         Label(window, text="Username: ").grid(row=1, column=0, sticky=E)
 
-        self.username = StringVar()
-        self.server = Entry(window).grid(row=1, column=1, sticky=W)
+       # self.username = StringVar()
+        self.username = Entry(window)
+        self.username.grid(row=1, column=1, sticky=W)
 
         Label(window, text="Password: ").grid(row=2, column=0, sticky=E)
 
-        self.password = StringVar()
-        self.server = Entry(window).grid(row=2, column=1, sticky=W)
+        #self.password = StringVar()
+        self.password = Entry(window, show='*')
+        self.password.grid(row=2, column=1, sticky=W)
 
         Button(window, text="Enter",
-               command=self.connect()).grid(row=3, column=0, sticky=S)
+               command=lambda: self.connect()
+               ).grid(row=3, column=0, sticky=S)
 
     def connect(self):
+        # calls the ubuntuConnection.py
+        oc.makeConnection(self.username.get(), self.password.get(), self.ip.get())
         self.display()
 
     def display(self):
-        msg = "Connected to ubuntu@" + self.server
+        msg = "Connected to ubuntu@" + self.ip.get() + " as " + self.username.get()
 
-        connectMsg = Text(self, height=1, width=200).grid()
-        #connectMsg.insert(END, "Connected to ubuntu@" + self.location)
+        connectMsg = Text(self, height=1, width=150)
+        connectMsg.grid()
+        connectMsg.insert(END, "Connected to ubuntu@" + self.location)
