c1eb87017195a3795251189346453354436de5ea
jhowell1 <56095374+jhowell1@users.noreply.github.com>
2019-11-08 18:17:17 -0800

    Update oracleConnection.py

--- oracleConnection.py
+++ oracleConnection.py
@@ -1,9 +1,13 @@
 import cx_Oracle
 
-conn_str = u'user101/pass101@54.70.62.21:1521'
-connection = cx_Oracle.connect(conn_str)
-c = connection.cursor()
-c.execute('select * from airlines')
-for row in c:
-    print(row)
-connection.close()
+
+def makeConnection (username, password, ip):
+    conn_str = username + '/' + password + '@' + ip + ':1521'
+    connection = cx_Oracle.connect(conn_str)
+    c = connection.cursor()
+    c.execute('select * from airlines')
+    #list = []
+    for row in c:
+        #list.append(row)
+        print(row)
+    connection.close()
