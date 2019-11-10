d90845126c4458bf01f79000fe0ae4df9e72beec
jhowell1 <56095374+jhowell1@users.noreply.github.com>
2019-11-09 16:00:48 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from tkinter import *
 from tkinter import ttk
 import ubuntuConnector as UC
+import serverOperations as SO
 
+# Establishes root GUI Frame as Tk() object (a GUI)
 root = Tk()
 root.title("Ubuntu Operator - James Howell and Bretton Florek")
+# Instantiate UbuntuConnector object with root as it's master window
 connection = UC.UbuntuConnector(root)
-nb = ttk.Notebook(root, width=800)
+# Instantiate a ttk Notebook object with root as it's master window
+nb = ttk.Notebook(root, width=1000)
+# Create Tabs in Notebook as new Frames
 f1 = ttk.Frame(nb)
 f2 = ttk.Frame(nb)
-#f3 = ttk.Frame(nb)
-#f4 = ttk.Frame(nb)
+# Add Tabs to Notebook with titles
 nb.add(f1, text="SQL Access")
 nb.add(f2, text="Make Comparisons")
-#nb.add(f3, text="Download Raw Data")
-#nb.add(f4, text="Add New Data")
+# Instantiate SQLAccess object with Notebook Tab 1 as it's master window
+SO.SQLAccess(f1)
 nb.grid()
-
+# Loop to keep program running until user closes program
 root.mainloop()
